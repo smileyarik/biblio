8d47e46401c9b606cbfd9e5917af209fe83e7771
Vadim Lyubanenko <lubanenko@yandex.ru>
2021-10-13 23:57:45 +0300

    refactor run.sh

--- run.sh
+++ run.sh
@@ -1,67 +1,73 @@
-ver="001"
+#!/bin/bash
 
-cat ../src_data/circulaton_1.csv | head -1 > ../data/circulation.csv
-cat ../src_data/circulaton_* | iconv -f cp1251 -t utf8 | grep -v circulationID >> ../data/circulation.csv
-cat ../src_data/cat_1.csv | head -1 > ../data/cat.csv
-cat ../src_data/cat_* | iconv -f cp1251 -t utf8 | grep -v "recId;" >> ../data/cat.csv
-cat ../src_data/fund_1.csv | head -1 > ../data/fund.csv
-cat ../src_data/fund_* | iconv -f cp1251 -t utf8 | grep -v catalogueRecordID >> ../data/fund.csv
-cat ../src_data/readers.csv | iconv -f cp1251 -t utf8 > ../data/readers.csv
-cat ../src_data/bookpoints.csv | iconv -f cp1251 -t utf8 > ../data/bookpoints.csv
-cat ../src_data/rubrics.csv | iconv -f cp1251 -t utf8 > ../data/rubrics.csv
-cat ../src_data/series.csv | iconv -f cp1251 -t utf8 > ../data/series.csv
-cat ../src_data/persons.csv | iconv -f cp1251 -t utf8 > ../data/persons.csv
-cat ../src_data/authors.csv | iconv -f cp1251 -t utf8 > ../data/authors.csv
+FULL_TRAIN=0
+INPUT_DIR="../src_data"
+TMP_DIR="../data"
 
-#python3 split.py ../data/circulation.csv ../data/minitr_st_circ.csv ../data/minitr_tg_circ.csv ../data/minivl_st_circ.csv ../data/minivl_tg_circ.csv ../data/minitt_st_circ.csv 01.10.2019 03.10.2019 03.10.2019 05.10.2019
-python3 split.py ../data/circulation.csv ../data/tr_st_circ.csv ../data/tr_tg_circ.csv ../data/vl_st_circ.csv ../data/vl_tg_circ.csv ../data/tt_st_circ.csv 17.08.2021 24.08.2021 24.08.2021 31.08.2021
+mkdir -p $TMP_DIR
 
-#python3 make_profiles.py ../data/bookpoints.csv ../src_data/books.jsn ../data/readers.csv ../data/minitr_tg_circ.csv ../data/minitr_st_circ.csv ../data/minitr_user_pickle.bin ../data/minitr_item_pickle.bin
-#python3 make_profiles.py ../data/bookpoints.csv ../src_data/books.jsn ../data/readers.csv ../data/minivl_tg_circ.csv ../data/minivl_st_circ.csv ../data/minivl_user_pickle.bin ../data/minivl_item_pickle.bin
-python3 make_profiles.py ../data/bookpoints.csv ../src_data/books.jsn ../data/readers.csv ../data/tr_tg_circ.csv ../data/tr_st_circ.csv ../data/tr_user_pickle.bin ../data/tr_item_pickle.bin
-python3 make_profiles.py ../data/bookpoints.csv ../src_data/books.jsn ../data/readers.csv ../data/vl_tg_circ.csv ../data/vl_st_circ.csv ../data/vl_user_pickle.bin ../data/vl_item_pickle.bin
+if [ "$OSTYPE" == "darwin"* ]; then
+    awk=gawk
+fi
 
-#python3 learn_rw.py 01.09.2019 ../data/minitr_tg_circ.csv ../data/minitr_st_circ.csv ../data/minitr_rw.bin
-#python3 learn_rw.py 01.10.2019 ../data/minivl_tg_circ.csv ../data/minivl_st_circ.csv ../data/minivl_rw.bin
-python3 learn_rw.py 17.02.2021 ../data/tr_tg_circ.csv ../data/tr_st_circ.csv ../data/tr_rw.bin
-python3 learn_rw.py 24.02.2021 ../data/vl_tg_circ.csv ../data/vl_st_circ.csv ../data/vl_rw.bin
+if [ FULL_TRAIN == 1 ]; then
+    TRAIN_RW_START_DATE="17.02.2021" # 6 month for statistics till TRAIN_START_DATE
+    TRAIN_START_DATE="17.08.2021"
+    TRAIN_END_DATE="24.08.2021"
+    VALID_RW_START_DATE="24.02.2021"
+    VALID_START_DATE="24.08.2021"
+    VALID_END_DATE="31.08.2021"
+else
+    TRAIN_RW_START_DATE="27.09.2019"
+    TRAIN_START_DATE="01.10.2019"
+    TRAIN_END_DATE="03.10.2019"
+    VALID_RW_START_DATE="01.10.2019"
+    VALID_START_DATE="03.10.2019"
+    VALID_END_DATE="05.10.2019"
+fi
 
+echo ">>> $TRAIN_START_DATE..$TRAIN_END_DATE, $VALID_START_DATE..$VALID_END_DATE"
 
-#python3 make_features.py ../data/minitr_user_pickle.bin ../data/minitr_item_pickle.bin ../data/minitr_rw.bin 50 100 01.10.2019 ../data/minitr_tg_circ.csv ../data/minitr_features.txt ../data/minitr_cd.txt ../data/minitr_st_circ.csv
-#python3 make_features.py ../data/minivl_user_pickle.bin ../data/minivl_item_pickle.bin ../data/minivl_rw.bin 50 100 03.10.2019 ../data/minivl_tg_circ.csv ../data/minivl_features.txt ../data/minivl_cd.txt ../data/minivl_st_circ.csv
-python3 make_features.py ../data/tr_user_pickle.bin ../data/tr_item_pickle.bin ../data/tr_rw.bin 100 200 17.07.2021 ../data/tr_tg_circ.csv ../data/tr_features.txt ../data/tr_cd.txt ../data/tr_st_circ.csv
-python3 make_features.py ../data/vl_user_pickle.bin ../data/vl_item_pickle.bin ../data/vl_rw.bin 100 200 24.08.2021 ../data/vl_tg_circ.csv ../data/vl_features.txt ../data/vl_cd.txt ../data/vl_st_circ.csv
+python3 split.py $INPUT_DIR/circulation.csv $TMP_DIR/tr_st_circ.csv $TMP_DIR/tr_tg_circ.csv $TMP_DIR/vl_st_circ.csv $TMP_DIR/vl_tg_circ.csv $TMP_DIR/tt_st_circ.csv $TRAIN_START_DATE $TRAIN_END_DATE $VALID_START_DATE $VALID_END_DATE
+
+python3 make_profiles.py $INPUT_DIR/bookpoints.csv $INPUT_DIR/books.jsn $INPUT_DIR/readers.csv $TMP_DIR/tr_tg_circ.csv $TMP_DIR/tr_st_circ.csv $TMP_DIR/tr_user_pickle.bin $TMP_DIR/tr_item_pickle.bin
+python3 make_profiles.py $INPUT_DIR/bookpoints.csv $INPUT_DIR/books.jsn $INPUT_DIR/readers.csv $TMP_DIR/vl_tg_circ.csv $TMP_DIR/vl_st_circ.csv $TMP_DIR/vl_user_pickle.bin $TMP_DIR/vl_item_pickle.bin
+
+python3 learn_rw.py $TRAIN_RW_START_DATE $TMP_DIR/tr_tg_circ.csv $TMP_DIR/tr_st_circ.csv $TMP_DIR/tr_rw.bin
+python3 learn_rw.py $VALID_RW_START_DATE $TMP_DIR/vl_tg_circ.csv $TMP_DIR/vl_st_circ.csv $TMP_DIR/vl_rw.bin
+
+python3 make_features.py $TMP_DIR/tr_user_pickle.bin $TMP_DIR/tr_item_pickle.bin $TMP_DIR/tr_rw.bin 100 200 $TRAIN_START_DATE $TMP_DIR/tr_tg_circ.csv $TMP_DIR/tr_features.txt $TMP_DIR/tr_cd.txt $TMP_DIR/tr_st_circ.csv
+python3 make_features.py $TMP_DIR/vl_user_pickle.bin $TMP_DIR/vl_item_pickle.bin $TMP_DIR/vl_rw.bin 100 200 $VALID_START_DATE $TMP_DIR/vl_tg_circ.csv $TMP_DIR/vl_features.txt $TMP_DIR/vl_cd.txt $TMP_DIR/vl_st_circ.csv
 
 # VANILLA
-#./catboost fit -f ../data/minitr_features.txt -t ../data/minivl_features.txt -m ../data/minitr_model.bin --cd ../data/minitr_cd.txt --loss-function YetiRank:decay=0.95 -i 500 --learn-err-log ../data/minitr_learn_error.txt --fstr-file ../data/minitr_model.fstr --fstr-type FeatureImportance --fstr-internal-file ../data/minitr_model.fstr_int --test-err-log ../data/minivl_learn_error.txt
-./catboost fit -f ../data/tr_features.txt -t ../data/vl_features.txt -m ../data/tr_model.bin --cd ../data/tr_cd.txt --loss-function YetiRank:decay=0.95 -i 500 --learn-err-log ../data/tr_learn_error.txt --fstr-file ../data/tr_model.fstr --fstr-type FeatureImportance --fstr-internal-file ../data/tr_model.fstr_int --test-err-log ../data/vl_learn_error.txt
+catboost fit -f $TMP_DIR/tr_features.txt -t $TMP_DIR/vl_features.txt -m $TMP_DIR/tr_model.bin --cd $TMP_DIR/tr_cd.txt --loss-function YetiRank:decay=0.95 -i 500 --learn-err-log $TMP_DIR/tr_learn_error.txt --fstr-file $TMP_DIR/tr_model.fstr --fstr-type FeatureImportance --fstr-internal-file $TMP_DIR/tr_model.fstr_int --test-err-log $TMP_DIR/vl_learn_error.txt
 
 # calc metrics
-./catboost calc -m ../data/tr_model.bin --cd ../data/tr_cd.txt -o ../data/vl_output.txt --input-path ../data/vl_features.txt
-cat ../data/vl_output.txt | awk 'NR>1' > ../data/a_vl.txt;
-cat ../data/vl_features.txt | awk -vOFS='\t' '{print $1,$2}' > ../data/b_vl.txt
-paste ../data/b_vl.txt ../data/a_vl.txt > ../data/c_vl.txt
-python3 make_result.py ../data/c_vl.txt 3 > ../data/answers_vl.txt
+catboost calc -m $TMP_DIR/tr_model.bin --cd $TMP_DIR/tr_cd.txt -o $TMP_DIR/vl_output.txt --input-path $TMP_DIR/vl_features.txt
 
-cat ../data/vl_features.txt | awk -vOFS='\t' '{print $1,$2,$4*1000000+$24}' > ../data/bb_vl.txt
-paste ../data/bb_vl.txt ../data/a_vl.txt > ../data/cc_vl.txt
-python make_result.py ../data/cc_vl.txt 2 > ../data/answers_pr_vl.txt
 
-cat ../data/vl_features.txt | awk -vOFS='\t' '{print $1,$2,$24}' > ../data/bb_vl.txt
-paste ../data/bb_vl.txt ../data/a_vl.txt > ../data/cc_vl.txt
-python make_result.py ../data/cc_vl.txt 2 > ../data/answers_pop_vl.txt
+cat $TMP_DIR/vl_output.txt | awk 'NR>1' > $TMP_DIR/a_vl.txt;
+cat $TMP_DIR/vl_features.txt | gawk -vOFS='\t' '{print $1,$2}' > $TMP_DIR/b_vl.txt
+paste $TMP_DIR/b_vl.txt $TMP_DIR/a_vl.txt > $TMP_DIR/c_vl.txt
+python3 make_result.py $TMP_DIR/c_vl.txt 3 > $TMP_DIR/answers_vl.txt
+
+cat $TMP_DIR/vl_features.txt | gawk -vOFS='\t' '{print $1,$2,$4*1000000+$24}' > $TMP_DIR/bb_vl.txt
+paste $TMP_DIR/bb_vl.txt $TMP_DIR/a_vl.txt > $TMP_DIR/cc_vl.txt
+python3 make_result.py $TMP_DIR/cc_vl.txt 2 > $TMP_DIR/answers_pr_vl.txt
+
+cat $TMP_DIR/vl_features.txt | gawk -vOFS='\t' '{print $1,$2,$24}' > $TMP_DIR/bb_vl.txt
+paste $TMP_DIR/bb_vl.txt $TMP_DIR/a_vl.txt > $TMP_DIR/cc_vl.txt
+python3 make_result.py $TMP_DIR/cc_vl.txt 2 > $TMP_DIR/answers_pop_vl.txt
 
 
 echo "Catboost"
-python3 split_answer.py ../data/vl_user_pickle.bin ../data/answers_vl.txt ../data/answers_vl_ ../data/vl_tg_circ.csv ../data/truth_vl_
-for h in -1 0 1 2 3 4 5 6 7 8 9 10; do echo "= $h =";python3.9 score.py ../data/answers_vl_$h.txt ../data/truth_vl_$h.txt; done
+python3 split_answer.py $TMP_DIR/vl_user_pickle.bin $TMP_DIR/answers_vl.txt $TMP_DIR/answers_vl_ $TMP_DIR/vl_tg_circ.csv $TMP_DIR/truth_vl_
+for h in -1 0 1 2 3 4 5 6 7 8 9 10; do echo "= $h =";python3 score.py $TMP_DIR/answers_vl_$h.txt $TMP_DIR/truth_vl_$h.txt; done
 
 echo "Pagerank"
-python3 split_answer.py ../data/vl_user_pickle.bin ../data/answers_pr_vl.txt ../data/answers_pr_vl_ ../data/vl_tg_circ.csv ../data/truth_vl_
-for h in -1 0 1 2 3 4 5 6 7 8 9 10; do echo "= $h =";python3.9 score.py ../data/answers_pr_vl_$h.txt ../data/truth_vl_$h.txt; done
+python3 split_answer.py $TMP_DIR/vl_user_pickle.bin $TMP_DIR/answers_pr_vl.txt $TMP_DIR/answers_pr_vl_ $TMP_DIR/vl_tg_circ.csv $TMP_DIR/truth_vl_
+for h in -1 0 1 2 3 4 5 6 7 8 9 10; do echo "= $h =";python3 score.py $TMP_DIR/answers_pr_vl_$h.txt $TMP_DIR/truth_vl_$h.txt; done
 
 echo "Item popularity"
-python3 split_answer.py ../data/vl_user_pickle.bin ../data/answers_pop_vl.txt ../data/answers_pop_vl_ ../data/vl_tg_circ.csv ../data/truth_vl_
-for h in -1 0 1 2 3 4 5 6 7 8 9 10; do echo "= $h =";python3.9 score.py ../data/answers_pop_vl_$h.txt ../data/truth_vl_$h.txt; done
-
-
+python3 split_answer.py $TMP_DIR/vl_user_pickle.bin $TMP_DIR/answers_pop_vl.txt $TMP_DIR/answers_pop_vl_ $TMP_DIR/vl_tg_circ.csv $TMP_DIR/truth_vl_
+for h in -1 0 1 2 3 4 5 6 7 8 9 10; do echo "= $h =";python3 score.py $TMP_DIR/answers_pop_vl_$h.txt $TMP_DIR/truth_vl_$h.txt; done
