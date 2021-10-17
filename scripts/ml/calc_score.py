256eb38455be151898b4230d7c6926a7074fbe04
Vadim Lyubanenko <lubanenko@yandex.ru>
2021-10-17 23:57:30 +0500

    site score

--- scripts/ml/calc_score.py
+++ scripts/ml/calc_score.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from collections import defaultdict
 from statistics import mean
+from util import is_site_user
 
 
 def precision_at(predictions, labels, k):
@@ -25,19 +26,32 @@ def MAP(predictions, labels, k=10):
     ])
 
 
-def main(input_directory, input_path):
-    predictions = defaultdict(list)
-    labels = defaultdict(list)
-    with open(os.path.join(input_directory, input_path)) as r:
-        for line in r:
-            user_id, item_id, label, prediction = line.strip().split("\t")
-            predictions[user_id].append(float(prediction))
-            labels[user_id].append(int(label))
+def print_metrics(predictions, labels):
     print("MAP@1:", MAP(predictions, labels, k=1))
     print("MAP@5:", MAP(predictions, labels, k=5))
     print("MAP@10:", MAP(predictions, labels, k=10))
     print("MAP@50:", MAP(predictions, labels, k=50))
     print("MAP@100:", MAP(predictions, labels, k=100))
+
+
+def main(input_directory, input_path):
+    site_predictions = defaultdict(list)
+    site_labels = defaultdict(list)
+    biblio_predictions = defaultdict(list)
+    biblio_labels = defaultdict(list)
+    with open(os.path.join(input_directory, input_path)) as r:
+        for line in r:
+            user_id, item_id, label, prediction = line.strip().split("\t")
+            if is_site_user(user_id):
+                site_predictions[user_id].append(float(prediction))
+                site_labels[user_id].append(int(label))
+            else:
+                biblio_predictions[user_id].append(float(prediction))
+                biblio_labels[user_id].append(int(label))
+    print("----- biblio -----")
+    print_metrics(biblio_predictions, biblio_labels)
+    print("----- site -----")
+    print_metrics(site_predictions, site_labels)
 
 
 if __name__ == "__main__":
