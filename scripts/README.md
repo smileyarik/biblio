2bcc0aef11c816bce751d12fa37c20ef4da4b66b
Vadim Lyubanenko <lubanenko@yandex.ru>
2021-10-17 22:56:19 +0500

    revert 2 last

--- scripts/README.md
+++ scripts/README.md
@@ -3,8 +3,3 @@
 bash +x download_data.sh ../../data
 bash +x refine_data.sh ../../data ../../refined_data
 ```
-
-### Prepare dataset and train model
-```
-bash +x make_dataset.sh ../../refined_data ../../workspace
-```
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,18 +1,39 @@
-#!/bin/bash
+!/bin/bash
 
 DIR="data_new"
 FULL_TRAIN=1
 
 if [ FULL_TRAIN == 1 ]; then
     TRAIN_PAGERANK_TS=1613520000
+    TRAIN_START_TS=1629158400
+    TRAIN_FINISH_TS=1629763200
     VALID_PAGERANK_TS=1614124800
+    VALID_START_TS=1629763200
+    VALID_FINISH_TS=1630368000
 else
     TRAIN_PAGERANK_TS=1569542400
+    TRAIN_START_TS=1569888000
+    TRAIN_FINISH_TS=1570060800
     VALID_PAGERANK_TS=1569888000
+    VALID_START_TS=1570060800
+    VALID_FINISH_TS=1570233600
 fi
 
 
 mkdir -p $DIR
+
+python3 -m ml.split_actions \
+    --input-directory $DIR \
+    --actions-path actions.jsonl \
+    --start-train-ts $TRAIN_START_TS \
+    --finish-train-ts $TRAIN_FINISH_TS \
+    --start-valid-ts $VALID_START_TS \
+    --finish-valid-ts $VALID_FINISH_TS \
+    --train-stat-path train_stat.jsonl \
+    --train-target-path train_target.jsonl \
+    --valid-stat-path valid_stat.jsonl \
+    --valid-target-path valid_target.jsonl \
+    --test-stat-path test_stat.jsonl
 
 python3 -m ml.make_profiles \
     --input-directory $DIR \
