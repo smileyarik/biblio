a4396fe07fe7aee11e5e550e3649eb41dbeaab08
Vadim Lyubanenko <lubanenko@yandex.ru>
2021-10-17 22:01:50 +0500

    download_data.sh

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+### Download and refine data
+```
+bash +x download_data.sh ../../data
+bash +x refine_data.sh ../../data ../../refined_data
+```
--- scripts/refine_data.sh
+++ scripts/refine_data.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-TEMP_DIR="temp"
-OUTPUT_DIR="$1"
+INPUT_DIR="$1"
+OUTPUT_DIR="$2"
 
-mkdir -p $TEMP_DIR
+if [ ! -d "$INPUT_DIR" ]
+then
+    echo "Directory $INPUT_DIR DOES NOT exists."
+    exit 1
+fi
+
 mkdir -p $OUTPUT_DIR
 
-echo "-----downloads-----"
-#cd $TEMP_DIR && wget https://www.dropbox.com/s/se6zn98ulv3ga0w/other_actions.tar.gz && tar -xzvf other_actions.tar.gz && cd ..
-#cd $TEMP_DIR && wget https://www.dropbox.com/s/lgp8z4jhqacf91v/all_books.jsonl.tar.gz && tar -xzvf all_books.jsonl.tar.gz && cd ..
-#cd $TEMP_DIR && wget https://www.dropbox.com/s/gl5z0s4q7h9kyll/actions.csv && cd ..
-#cd $TEMP_DIR && wget https://www.dropbox.com/s/b2adss0k8n65stx/cat_3.csv && cd ..
-#cd $TEMP_DIR && wget https://www.dropbox.com/s/nrositd7ceqid7p/circulaton_3.csv && cd ..
-#cd $TEMP_DIR && wget https://www.dropbox.com/s/bm87xst2bn084t8/items.json && cd ..
-
 echo "-----items-----"
-python3 -m prepare.refine_items --input-directory $TEMP_DIR --output-path "$OUTPUT_DIR/items.jsonl"
-echo "-----users-----"
-python3 -m prepare.refine_users --input-directory $TEMP_DIR --output-path "$OUTPUT_DIR/users.jsonl"
-echo "-----actions-----"
-python3 -m prepare.refine_actions --input-directory $TEMP_DIR --output-path "$OUTPUT_DIR/actions.jsonl" --refined-items-path "$OUTPUT_DIR/items.jsonl" --refined-users-path "$OUTPUT_DIR/users.jsonl"
+python3 -m prepare.refine_items --input-directory $INPUT_DIR --output-path "$OUTPUT_DIR/items.jsonl"
 
-#rm -rf $TEMP_DIR
+echo "-----users-----"
+python3 -m prepare.refine_users --input-directory $INPUT_DIR --output-path "$OUTPUT_DIR/users.jsonl"
+
+echo "-----actions-----"
+python3 -m prepare.refine_actions --input-directory $INPUT_DIR --output-path "$OUTPUT_DIR/actions.jsonl" --refined-items-path "$OUTPUT_DIR/items.jsonl" --refined-users-path "$OUTPUT_DIR/users.jsonl"
