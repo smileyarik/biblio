a23a0b2debc0fde6fe97821a727f8dd9d7117253
Ilya Gusev <phoenixilya@gmail.com>
2021-10-24 15:59:00 +0000

    fix

--- scripts/ml/make_profiles.py
+++ scripts/ml/make_profiles.py
@@ -96,12 +96,7 @@ def main(
 ):
     item_profiles = dict()
     user_profiles = dict()
-
     target_user_ids = set()
-    def _is_target_user(user_id):
-        if make_for_site:
-            return is_site_user(user_id)
-        return not target_user_ids or user_id in target_user_ids
 
     print("Init BBK Processor")
     bbk_processor = BbkProcessor(bbk_path)
@@ -113,6 +108,11 @@ def main(
             target_user_ids.add(action["user_id"])
     else:
         print("... omitted")
+
+    def _is_target_user(user_id):
+        if make_for_site:
+            return is_site_user(user_id)
+        return not target_user_ids or user_id in target_user_ids
 
     print("Read books data")
     item_gen = read_jsonl(os.path.join(input_directory, items_path))
@@ -149,7 +149,7 @@ def main(
 
         for rt in [RT.SUM, RT.D7, RT.D30]:
             item_profile.counters.add(OT.GLOBAL, CT.BOOKING, rt, '', 1, ts)
-            if _is_target_user(user_id):
+            if not _is_target_user(user_id):
                 continue
 
             user_profile.counters.add(OT.GLOBAL, CT.BOOKING, rt, '', 1, ts)
